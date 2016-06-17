6271bb8051546c8bdb3cc225fe97eaaf3d8af4bf
Joe Wass <jwass@crossref.org>
2016-06-17 11:42:54 +0100

    First commit.

---
+++ README.md
@@ -0,0 +1,9 @@
+# Baleen
+
+A library to help writing Event Data agents. 
+
+## License
+
+Copyright © 2016 Crossref
+
+Distributed under the MIT License.
---
+++ test/baleen/core_test.clj
@@ -0,0 +1,7 @@
+(ns baleen.core-test
+  (:require [clojure.test :refer :all]
+            [baleen.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
